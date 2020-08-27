bc2885b242d8470d07248f2dc39c3ede6267fa19
DonovanFoley <66860625+DonovanFoley@users.noreply.github.com>
2020-08-26 20:54:10 -0700

    Add files via upload

---
+++ CS1C Lab 3/CS1C Lab 3.cpp
@@ -0,0 +1,17 @@
+// Donovan Foley and Nathan Kim
+// Description: Hello World in C++
+//================================= main
+
+#include <iostream>
+
+using namespace std;
+
+int hello1Function(string name1);
+
+int main()
+{
+	string name1 = "your name1";
+	hello1Function(name1);
+
+	return 0;
+}
